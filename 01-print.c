b30ba5512b183001974f96dad1e6a1793961aa85
Aleyna Kaya <devaleykaya@gmail.com>
2024-07-20 16:53:28 +0300

    printf fonksiyonu

---
+++ 01-print.c
@@ -0,0 +1,8 @@
+# include <stdio.h> //Gerekli kütüphaneyi dahil ediyorum
+
+int main() {
+    // "Merhaba, Dünya!\n" mesajını ekrana yazdırıyorum
+    printf("Merhaba, Dünya\n");
+    printf("C programlama öğreniyorum.\n");
+    return 0; // Programı başarılı bir şekilde sonlandırıyorum
+}
